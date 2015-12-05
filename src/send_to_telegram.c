4c1ebd377c67334deccda4dcd38f697b53adb299
Alex <you@example.com>
2015-12-05 16:53:13 +0300

    Find chat id

--- src/send_to_telegram.c
+++ src/send_to_telegram.c
@@ -9,44 +9,50 @@
 
 static gint find_chat_id(gchar *bot_tocken, gchar *acc_name);
 
-static void read_telegram_config() {
-	GSettings *settings = g_settings_new ("org.gnome.gnome-screenshot");
-	gchar *bot_tocken = g_settings_get_string(settings, "bot-tocken");
-	gchar *acc_name = g_settings_get_string(settings, "acc-name");
-	gint chat_id = g_settings_get_int(settings, "chat-id");
-	if (chat_id || (chat_id = find_chat_id(bot_tocken, acc_name))) {
-		g_printerr("chat_id:");
-	} else {
-		// g_printerr("can't find chat_id");
-	}
-	// g_settings_set_int(settings, "chat-id", )
-}
-
 
 #define BUFF_LEN 512
+gchar buff[BUFF_LEN];
+
+struct chat_data {
+	gint chat_id;
+	gchar *acc_name;
+};
 
 static size_t WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp)
 {
 	size_t realsize = size * nmemb;
-	gchar *buf = (gchar *)userp;
+	struct chat_data *cd = (struct chat_data*)userp;
 
-	if (realsize > BUFF_LEN * sizeof(gchar)) {
-		fprintf(stderr, "not enough memory");
-		return 0;
+	JsonParser *parser;
+	JsonNode *root;
+	parser = json_parser_new ();
+	json_parser_load_from_data (parser, (gchar *)contents, realsize, NULL);
+	root = json_parser_get_root (parser);
+	JsonObject *chat_object = json_object_get_object_member(
+	                              json_object_get_object_member(
+	                                  json_array_get_object_element(
+	                                      json_object_get_array_member(json_node_get_object(root), "result"),
+	                                      0), "message"), "chat");
+	gchar *last_name = json_object_get_string_member(chat_object , "last_name");
+	gchar *first_name = json_object_get_string_member(chat_object , "first_name");
+
+	sprintf(buff, "%s %s", first_name, last_name);
+	if (!g_strcmp0(buff, cd->acc_name)) {
+		cd->chat_id = json_object_get_int_member(chat_object, "id");
 	}
-
-	memcpy(buf, contents, realsize);
-	// printf("%s\n", buf);
+	printf("%s\n", buff);
+	g_object_unref (parser);
 
 	return realsize;
 }
+
 
 static gint find_chat_id(gchar *bot_tocken, gchar *acc_name) {
 	CURL *curl;
 	CURLcode res;
 
-	gchar response[BUFF_LEN];
 	curl = curl_easy_init();
+	struct chat_data data = {0, acc_name};
 
 	if (curl) {
 		gchar url[256];
@@ -54,7 +60,7 @@ static gint find_chat_id(gchar *bot_tocken, gchar *acc_name) {
 		curl_easy_setopt(curl, CURLOPT_URL, url);
 		curl_easy_setopt(curl, CURLOPT_POST, 0);
 		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
-		curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)response);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&data);
 		res = curl_easy_perform(curl);
 
 		/* Check for errors */
@@ -64,17 +70,42 @@ static gint find_chat_id(gchar *bot_tocken, gchar *acc_name) {
 
 		/* always cleanup */
 		curl_easy_cleanup(curl);
-
+		return data.chat_id;
 	}
 }
 
 
 
-static void send_photo(int chat_id) {
+struct TConfig
+{
+	GSettings *settings;
+	gchar *bot_tocken;
+	gchar *acc_name;
+	gint chat_id;
+};
+
+struct TConfig config;
+
+static void read_telegram_config() {
+	config.settings = g_settings_new ("org.gnome.gnome-screenshot");
+	config.bot_tocken = g_settings_get_string(config.settings, "bot-tocken");
+	config.acc_name = g_settings_get_string(config.settings, "acc-name");
+	config.chat_id = g_settings_get_int(config.settings, "chat-id");
+	gint new_chat_id;
+	if (config.chat_id || (new_chat_id = find_chat_id(config.bot_tocken, config.acc_name))) {
+		if (!config.chat_id) {
+			g_settings_set_int(config.settings, "chat-id", new_chat_id);
+			config.chat_id = new_chat_id;
+		}
+	}
+}
+
+static void send_photo() {
 
 }
 
 int send_telegram() {
 	read_telegram_config();
+	send_photo();
 	return 0;
 }
